e6d452be41a64386c2a1cd0d3cb54e08d7c07526
root <root@vps419339.ovh.net>
2020-09-09 23:24:24 +0200

    fix(design): fix the colors of the loader

--- src/components/LoaderSpinner.js
+++ src/components/LoaderSpinner.js
@@ -9,7 +9,7 @@ const rotate360 = keyframes`
   }
 `;
 
-// rgb(162,17,10) is the value of the secondary color, and rgb(243,202,48) the value of the tertiary color. Please to apply one of the following modifications :
+// rgb(126,170,205) is the value of the secondary color, and rgb(232,92,93) the value of the lightred color. Please to apply one of the following modifications :
 // - modify the theme, so that we could use either the HEX or the RGB format of the colors, i.e we need to have the theme with the following format : theme = { primary : { hex: "...", rgb: "..." } }
 // - or we could just use a function to transform HEX to RGB. But it could slow down the app
 
@@ -21,10 +21,10 @@ const LoaderSpinner = styled.div`
   animation: ${rotate360} ${({ speed }) => speed} linear infinite;
   transform: translateZ(0);
 
-  border-top: 2px solid rgb(243, 202, 48);
-  border-right: 2px solid rgb(243, 202, 48);
-  border-bottom: 2px solid rgb(243, 202, 48);
-  border-left: 4px solid rgb(162, 17, 10);
+  border-top: 2px solid rgb(232, 92, 93);
+  border-right: 2px solid rgb(232, 92, 93);
+  border-bottom: 2px solid rgb(232, 92, 93);
+  border-left: 4px solid rgb(126, 170, 205);
   background: transparent;
   width: 24px;
   height: 24px;
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// TODO : move it in a helper
 const hexToRgb = (rawHex) => {
   // http://stackoverflow.com/a/5624139
   const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
@@ -8,18 +9,22 @@ const hexToRgb = (rawHex) => {
   });
 
   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  result = result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16),
-  } : null
-  return result ? `rgb(${result.r}, ${result.g}, ${result.b})` : 'rgb(0, 0 ,0)'
+  result = result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+  return result ? `rgb(${result.r}, ${result.g}, ${result.b})` : 'rgb(0, 0 ,0)';
 };
 
 const Paragraph = styled.p`
   ${({ size, theme }) => `
     border: 4px solid;
-    border-image: linear-gradient(45deg, ${hexToRgb(theme.tertiary)} 0%, ${hexToRgb(theme.tertiary)} 50%, ${hexToRgb(theme.lightred)} 100%) 1;
+    border-image: linear-gradient(45deg, ${hexToRgb(
+      theme.tertiary
+    )} 0%, ${hexToRgb(theme.tertiary)} 50%, ${hexToRgb(theme.lightred)} 100%) 1;
     color: ${theme.secondary};
     margin: 0px;
     font-size: ${size};
